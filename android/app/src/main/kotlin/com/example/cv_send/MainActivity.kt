bb9a381693ff502d5ca0e4855257916ac88f3edb
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2023-12-18 11:19:14 -0500

    Feat: build web

--- android/app/src/main/kotlin/com/example/cv_send/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cv_send
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/_childrens/init/listeners/core.dart
+++
@@ -1,7 +0,0 @@
-part of 'package:cv_send/_childrens/init/page.dart';
-
-void listener(BuildContext context, bloc.State state) {
-  if (state is bloc.LoadedState) {
-    XigoRoute.navHomeReplaceName();
-  }
-}
